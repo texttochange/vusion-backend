d17f916d7a0ea08a12eb589f46a578af1f0e8216
Olivier Vernin <overnin@gmail.com>
2012-02-28 18:47:13 +0300

    adding testing script

---
+++ run_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -e
+
+#cd "$(dirname "$0")/.."
+
+echo "=== Nuking old .pyc files..."
+find vusion/ -name '*.pyc' -delete
+find transports/ -name '*.pyc' -delete
+echo "=== Erasing previous coverage data..."
+coverage erase
+echo "=== Running trial tests..."
+coverage run `which trial` --reporter=subunit tests | tee results.txt | subunit2pyunit
+subunit2junitxml <results.txt >test_results.xml
+rm results.txt
+echo "=== Processing coverage data..."
+coverage xml
+echo "=== Checking for PEP-8 violations..."
+pep8 --repeat --exclude='migrations' vusion | tee pep8.txt
+echo "=== Done."
