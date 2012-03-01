42eeb0a9e7cf841d2ba12838e4c468ec0ae745ab
Olivier Vernin <overnin@gmail.com>
2012-03-01 17:05:08 +0300

    Correct Yo Transport test, implement receive SMS

--- tests/test_yo_ug_http.py
+++ tests/test_yo_ug_http.py
@@ -7,8 +7,9 @@ from twisted.web.resource import Resource
 
 from vumi.transports.tests.test_base import TransportTestCase
 from transports import YoUgHttpTransport
-from vumi.tests.utils import get_stubbed_worker, TestResourceWorker
-from vumi.message import from_json
+from vumi.tests.utils import get_stubbed_worker, TestResourceWorker, RegexMatcher, UTCNearNow
+from vumi.utils import http_request_full
+from vumi.message import TransportMessage, TransportEvent, TransportUserMessage
 
 class YoUgHttpTransportTestCase(TransportTestCase):
 
@@ -19,39 +20,122 @@ class YoUgHttpTransportTestCase(TransportTestCase):
     @inlineCallbacks
     def setUp(self):
         yield super(YoUgHttpTransportTestCase, self).setUp()
-        self.path = '/sendsms'
-        self.port = 9999
+        self.send_path = '/sendsms'
+        self.send_port = 9999
         self.config = {
             'transport_name': 'yo',
-            'url':'http://localhost:%s%s'%(self.port, self.path),
+            'url':'http://localhost:%s%s'%(self.send_port, self.send_path),
             'ybsacctno':'ybsacctno',
-            'password':'password',          
+            'password':'password',
+            'receive_path':'/yo',
+            'receive_port': 9998
         }
         self.worker = yield self.get_transport(self.config)
         self.today = datetime.utcnow().date()
 
+
+    def mkmsg_fail(self, user_message_id='1', 
+                  failure_level = '', failure_code = 0,
+                  failure_reason = '',
+                  transport_metadata = {}
+                  ):
+        if transport_metadata is None:
+            transport_metadata = {}
+        return TransportEvent(
+            event_id=RegexMatcher(r'^[0-9a-fA-F]{32}$'),
+            event_type='delivery_report',
+            delivery_status='failed',
+            failure_level = failure_level,
+            failure_code = failure_code,
+            failure_reason = failure_reason,
+            user_message_id=user_message_id,
+            timestamp=UTCNearNow(),
+            transport_name=self.transport_name,
+            transport_metadata=transport_metadata,
+            )
+
+    def mkmsg_in(self, content='Hello World',
+                 session_event=TransportUserMessage.SESSION_NONE,
+                 message_id='abc', transport_type=None,
+                 transport_metadata=None):
+        if transport_type is None:
+            transport_type = self.transport_type
+        if transport_metadata is None:
+            transport_metadata = {}
+        return TransportUserMessage(
+            from_addr='41791234567',
+            to_addr='9292',
+            group=None,
+            message_id=message_id,
+            transport_name=self.transport_name,
+            transport_type=transport_type,
+            transport_metadata=transport_metadata,
+            content=content,
+            session_event=session_event,
+            timestamp=UTCNearNow(),
+            )
+
+
     def make_resource_worker(self, msg, code=http.OK, send_id=None):
         w = get_stubbed_worker(TestResourceWorker, {})
         w.set_resources([
-            (self.path, TestResource, ( msg, code, send_id))])
+            (self.send_path, TestResource, ( msg, code, send_id))])
         self._workers.append(w)
         return w.startWorker()
     
     @inlineCallbacks
-    def test_sending_one_sms_ok(self):
+    def test01_sending_one_sms_ok(self):
         #mocked_message_id = str(uuid4())
         mocked_message = "ybs_autocreate_status=OK"
         #HTTP response
         yield self.make_resource_worker(mocked_message) 
         #Message to transport
-        yield self.dispatch(self.mkmsg_out())    
+        yield self.dispatch(self.mkmsg_out(to_addr='788601462'))    
         [smsg] = self.get_dispatched('yo.event')
         self.assertEqual(self.mkmsg_ack(),
-                         from_json(smsg.body))
+                         TransportMessage.from_json(smsg.body))
 
     @inlineCallbacks
-    def test_receiving_one_sms(self):
-        self.assertTrue(False)
+    def test02_sending_one_sms_http_failure(self):
+        mocked_message = "timeout"
+        mocked_error = http.REQUEST_TIMEOUT
+        
+        #HTTP response
+        yield self.make_resource_worker(mocked_message, mocked_error)        
+        yield self.dispatch(self.mkmsg_out(to_addr='256788601462'))    
+
+        [smsg] = self.get_dispatched('yo.event')
+        self.assertEqual(self.mkmsg_fail(
+            failure_level = 'http',
+            failure_code = http.REQUEST_TIMEOUT,
+            failure_reason = 'timeout'),
+                         TransportMessage.from_json(smsg.body))
+
+    @inlineCallbacks
+    def test03_sending_one_sms_service_failure(self):
+        mocked_message = "ybs_autocreate_status=ERROR&ybs_autocreate_message=YBS+AutoCreate+Subsystem%3A+Access+denied+due+to+wrong+authorization+code"
+
+        #HTTP response
+        yield self.make_resource_worker(mocked_message) 
+        yield self.dispatch(self.mkmsg_out(to_addr='788601462'))    
+        [smsg] = self.get_dispatched('yo.event')
+        self.assertEqual(self.mkmsg_fail(
+            failure_level = 'service',
+            failure_code = 'ERROR',
+            failure_reason = "YBS+AutoCreate+Subsystem%3A+Access+denied+due+to+wrong+authorization+code"),
+                         TransportMessage.from_json(smsg.body))
+    
+    @inlineCallbacks
+    def test04_receiving_one_sms(self):
+        url = "http://localhost:%s%s?sender=41791234567&code=9292&message=HelloWorld" % (self.config['receive_port'], 
+                                         self.config['receive_path'])
+        response = yield http_request_full(url, method='GET')
+        [smsg] = self.get_dispatched('yo.inbound')
+        
+        self.assertEqual(response.code, http.OK)
+        self.assertEqual(self.mkmsg_in(content='HelloWorld'),
+                         TransportMessage.from_json(smsg.body))
+        
     
     def get_dispatched(self, rkey):
         return self._amqp.get_dispatched('vumi', rkey)
@@ -66,9 +150,16 @@ class TestResource(Resource):
         self.send_id = send_id
 
     def render_GET(self, request):
-        #log.msg(request.content.read())
+        #log.msg(request.content.read()
         request.setResponseCode(self.code)
-        #log.msg('request.arg', request.args)
-        return self.message
+        if (not ('destinations' in request.args) or 
+            not ('origin' in request.args) or
+            not ('password' in request.args) or
+            not ('sms_content' in request.args) or
+            not ('ybsacctno' in request.args)
+            ):
+            return "ybs_autocreate_status=ERROR"
+        else:
+            return self.message
     
 
--- transports/yo_ug_http.py
+++ transports/yo_ug_http.py
@@ -5,6 +5,9 @@ from urllib import urlencode
 from twisted.python import log
 from twisted.internet.defer import inlineCallbacks
 from twisted.internet.error import ConnectionRefusedError
+from twisted.web import http
+from twisted.web.resource import Resource
+from twisted.web.server import NOT_DONE_YET
 
 from vumi.transports.base import Transport
 from vumi.utils import http_request_full, normalize_msisdn
@@ -12,9 +15,24 @@ from twisted.internet import defer
 defer.setDebugging(True)
 
 class YoUgHttpTransport(Transport):
+
+    def mkres(self, cls, publish_func, path_key):
+        resource = cls(self.config, publish_func)
+        self._resources.append(resource)
+        return (resource, self.config['receive_path'])
+
     
+    @inlineCallbacks
     def setup_transport(self):
+        self._resources = [];
         log.msg("Setup yo transport %s" % self.config)
+        #self.config.setdefault('receive_path', '/yo')
+        resources = [
+            self.mkres(ReceiveSMSResource, self.publish_message, '/receive')
+            ]
+        self.receipt_resource = yield self.start_web_resources(
+            resources, self.config['receive_port'])
+        
         
     @inlineCallbacks
     def handle_outbound_message(self, message):
@@ -36,20 +54,69 @@ class YoUgHttpTransport(Transport):
                 'GET')
         except ConnectionRefusedError:
             log.msg("Connection failed sending message: %s" % message)
-            raise TemporaryFailure('connection refused')
+            #raise TemporaryFailure('connection refused')
         except Exception as ex:
             log.msg("Unexpected error %s" % repr(ex))
-            
+                
         if response.code != 200:
-            log.msg("Error returned %s: %s" % (response.code, response.delivered_body))
-            raise PermanentFailure('server error: HTTP %s: %s'
-                                   % (response.code, response.delivered_body))
-        
+            log.msg("Http Error %s: %s" % (response.code, response.delivered_body))
+            yield self.publish_delivery_report(
+                user_message_id = message['message_id'],
+                delivery_status = 'failed',
+                failure_level = 'http',
+                failure_code = response.code,
+                failure_reason = response.delivered_body
+                )
+            return
+
+        ybs_autocreate_status = response.delivered_body.split("&")[0].split('=')[1]
+        if (ybs_autocreate_status=='ERROR'):
+            log.msg("Yo Error %s: %s" % (response.code, response.delivered_body))
+            yield self.publish_delivery_report(
+                user_message_id = message['message_id'],
+                delivery_status = 'failed',
+                failure_level = 'service',
+                failure_code = ybs_autocreate_status,
+                failure_reason = response.delivered_body.split("&")[1].split('=')[1]
+                )
+            return
+
         log.msg("Sms received and accepted by Yo %s" % response.delivered_body)
         yield self.publish_ack(
             user_message_id=message['message_id'],
             sent_message_id="abc",
         )
     
-    def stop_worker(self):
+    def stopWorker(self):
         log.msg("stop yo transport")
+        if hasattr(self, 'receipt_resource'):
+            return self.receipt_resource.stopListening()
+
+
+class ReceiveSMSResource(Resource):
+    isLeaf = True
+    
+    def __init__(self, config, publish_func):
+        self.config = config
+        self.publish_func = publish_func
+        self.transport_name = self.config['transport_name']
+    
+    @inlineCallbacks
+    def do_render(self, request):
+        log.msg('got hit with %s' % request.args)
+        request.setResponseCode(http.OK)
+        request.setHeader('Content-Type', 'text/plain')
+        yield self.publish_func(
+             transport_name = self.transport_name,
+                transport_type = 'sms',
+                message_id = 'abc',
+                to_addr = request.args['code'],
+                from_addr = normalize_msisdn(request.args['sender']),
+                content = request.args['message'],
+                transport_metadata = {}
+        )
+        request.finish()
+        
+    def render(self, request):
+        self.do_render(request)
+        return NOT_DONE_YET
