0991258113a871ddd763dcd7f12b846ad0beb63b
Olivier Vernin <overnin@gmail.com>
2012-04-04 11:53:59 +0300

    move the creation of consumer for startService into startWorker

--- vusion/multiworker.py
+++ vusion/multiworker.py
@@ -8,7 +8,6 @@ from vumi import log
 
 class VusionMultiWorker(MultiWorker):
 
-    @inlineCallbacks
     def startService(self):
         log.debug('Starting Multiworker %s' % (self.config,))
         super(MultiWorker, self).startService()
@@ -19,28 +18,35 @@ class VusionMultiWorker(MultiWorker):
             worker = self.create_worker(wname, wclass)
             self.workers['wname'] = worker
 
+    @inlineCallbacks
+    def startWorker(self):
         yield self.setup_control()
 
     @inlineCallbacks
     def setup_control(self):
         self.control = yield self.consume(
-        '%s.control' % self.config['application_name'],
+        '%s.control' % (self.config['application_name'],),
         self.receive_control_message,
         message_class=Message)
 
     def receive_control_message(self, msg):
         log.debug('Received control! %s' % (msg,))
-        
-        if msg['message_type'] == 'add_worker':
-            if msg['worker_name'] in self.workers:
-                log.error('Cannot create worker, another worker already exist with same name: %s' % (msg['worker_name'],))
-            for key in msg['config'].keys():
-                msg['config'][key] = msg['config'][key].encode('utf-8')
-            self.config[msg['worker_name']] = msg['config']
-            worker = self.create_worker(msg['worker_name'], msg['worker_class'])
-            self.workers[msg['worker_name']] = worker
 
-        if msg['message_type'] == 'remove_worker':
-            if not msg['worker_name'] in self.workers:
-                log.error('Cannot remove worker, no worker with this name: %s' % (msg['worker_name'],))
-            self.workers[msg['worker_name']].stopService()
+        try:
+            if msg['message_type'] == 'add_worker':
+                if msg['worker_name'] in self.workers:
+                    log.error('Cannot create worker, another worker already exist with same name: %s' % (msg['worker_name'],))
+                for key in msg['config'].keys():
+                    msg['config'][key] = msg['config'][key].encode('utf-8')
+                self.config[msg['worker_name']] = msg['config']
+                worker = self.create_worker(msg['worker_name'], msg['worker_class'])
+                self.workers[msg['worker_name']] = worker
+            
+            if msg['message_type'] == 'remove_worker':
+                if not msg['worker_name'] in self.workers:
+                    log.error('Cannot remove worker, no worker with this name: %s' % (msg['worker_name'],))
+                self.workers[msg['worker_name']].stopService()
+
+        except Exception as ex:
+            log.error("Control received: %s" % (msg))
+            log.error("Unexpected error %s" % repr(ex))
