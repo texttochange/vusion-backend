3f3c498971325050d37c0821a89df5fe00a94815
Olivier Vernin <overnin@gmail.com>
2012-11-27 17:54:06 +0300

    fix adding + to the phone number

--- transports/push_tz_smpp.py
+++ transports/push_tz_smpp.py
@@ -1,11 +1,15 @@
+import re
 from vumi.transports.smpp import SmppTransport
 
 class PushTzSmppTransport(SmppTransport):
+    
+    regex_plus = re.compile("^\+")
     
     def handle_outbound_message(self, message):
         message['from_addr'] = message['from_addr'] if 'default_origin' not in self.config else self.config['default_origin']
         super(PushTzSmppTransport, self).handle_outbound_message(message)
 
     def deliver_sm(self, *args, **kwargs):
-        kwargs.update({'source_addr': ('+%s' % kwargs.get('source_addr'))})        
+        if (not re.match(self.regex_plus, kwargs.get('source_addr'))):
+            kwargs.update({'source_addr': ('+%s' % kwargs.get('source_addr'))})        
         super(PushTzSmppTransport, self).deliver_sm(*args, **kwargs)
