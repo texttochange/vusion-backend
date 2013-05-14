c50dfbf34f00d8f8b33175b86bdd077acf1ffa93
Olivier Vernin <overnin@gmail.com>
2013-05-14 14:03:45 +0300

    refactor push smpp

--- transports/push_tz_smpp.py
+++ transports/push_tz_smpp.py
@@ -21,7 +21,6 @@ from smpp.pdu_inspector import (MultipartMessage,
 from vumi.message import TransportUserMessage
 from vumi.transports.smpp import SmppTransport
 from vumi.transports.smpp.clientserver.client import EsmeTransceiver, EsmeTransceiverFactory
-from vumi.middleware import MiddlewareStack, setup_middlewares_from_config
 
 from middlewares.custom_middleware_stack import CustomMiddlewareStack, useCustomMiddleware
 
@@ -124,57 +123,3 @@ class PushTzEsmeTransceiver(EsmeTransceiver):
                         short_message=decoded_msg,
                         message_id=message_id,
                         )
-
-#class MiddlewareControlFlag(Exception):
-    #pass
-
-
-#class StopPropagation(MiddlewareControlFlag):
-    #pass
-
-
-#class CustomMiddlewareStack(MiddlewareStack):
-
-    #def _get_middleware_index(self, middleware):
-        #return self.middlewares.index(middleware)
-
-    #@inlineCallbacks
-    #def resume_handling(self, mw, handle_name, message, endpoint):
-        #mw_index = self._get_middleware_index(mw)
-        ##In case there are no other middleware after this one
-        #if mw_index + 1 == len(self.middlewares):
-            #returnValue(message)
-        #message = yield self._handle(self.middlewares, handle_name, message, endpoint, mw_index + 1)
-        #returnValue(message)
-
-    #@inlineCallbacks
-    #def _handle(self, middlewares, handler_name, message, endpoint, from_index=0):
-        #method_name = 'handle_%s' % (handler_name,)
-        #middlewares = list(middlewares)
-        #if len(middlewares) == 0:
-            #returnValue(message)
-        #for index, middleware in enumerate(middlewares[from_index:]):
-            #handler = getattr(middleware, method_name)
-            #message = yield self._handle_middleware(handler, message, endpoint, index)
-            #if message is None:
-                #raise MiddlewareError('Returned value of %s.%s should never ' \
-                                #'be None' % (middleware, method_name,))
-        #returnValue(message)
-
-    #def _handle_middleware(self, handler, message, endpoint, index):        
-        #def _handle_control_flag(f):
-            #if not isinstance(f.value, MiddlewareControlFlag):
-                #raise f
-            #if isinstance(f.value, StopPropagation):
-                #raise f
-            #raise MiddlewareError('Unknown Middleware Control Flag: %s'
-                                  #% (f.value,))                
-        
-        #d = maybeDeferred(handler, message, endpoint)
-        #d.addErrback(_handle_control_flag)
-        #return d
-
-    #def process_control_flag(self, f):
-        #f.trap(StopPropagation)
-        #if isinstance(f.value, StopPropagation):
-            #return None
