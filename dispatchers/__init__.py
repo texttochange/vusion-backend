2955aefd3fcaf061aa49c6cea68be226d335ab56
Olivier Vernin <overnin@gmail.com>
2013-09-09 18:01:22 +0300

    fix dispatcher test

--- dispatchers/__init__.py
+++ dispatchers/__init__.py
@@ -1,3 +1,4 @@
 from ttc_dispatcher import DynamicDispatchWorker
+from priority_router import PriorityContentKeywordRouter
 
-__all__ = ["DynamicDispatchWorker"]
+__all__ = ["DynamicDispatchWorker", "PriorityContentKeywordRouter"]
