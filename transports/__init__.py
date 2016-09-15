3fc23712cdc0544fb912dac70b161b3753d289eb
mark <markphi119@gmail.com>
2016-09-15 15:10:48 +0300

    space

--- transports/__init__.py
+++ transports/__init__.py
@@ -12,7 +12,6 @@ from orange_mali.orange_mali_smpp import OrangeMaliSmppTransport
 from orange_sdp.orange_http import OrangeSdpHttpTransport
 from iconcept_nigeria.iconcept_http import IConceptHttpTransport
 from telkomsel_indonesia.telkomsel_http import TelkomselHttpTransport
-
 from http_forward.forward_http import ForwardHttp
 from http_forward.cioec_http import CioecHttp
 from apposit_ethiopia.apposit import AppositV2Transport
