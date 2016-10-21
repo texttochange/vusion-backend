4ed7cc45e01f78c97bdbd603b70914bf37a47d7b
mark <markphi119@gmail.com>
2016-10-21 09:15:06 +0300

    added module

--- transports/__init__.py
+++ transports/__init__.py
@@ -14,6 +14,7 @@ from iconcept_nigeria.iconcept_http import IConceptHttpTransport
 from telkomsel_indonesia.telkomsel_http import TelkomselHttpTransport
 from http_forward.forward_http import ForwardHttp
 from http_forward.cioec_http import CioecHttp
+
 from http_forward.askpeople_http import AskpeopleHttp
 from apposit_ethiopia.apposit import AppositV2Transport
 
